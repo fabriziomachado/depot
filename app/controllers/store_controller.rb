051e49bef8b8adb5384665d9eebbaec9ec22912c
fabrizio <fcm@unesc.net>
2011-06-02 20:30:54 -0300

    refactoring extract partial

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,7 @@ class StoreController < ApplicationController
   def index
     @count_access_store_index = increment_session_counter
     @products = Product.all
+    @cart = current_cart
   end
 
 end
