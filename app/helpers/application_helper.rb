a186ba035b15763d769ad3b2d015d8935e7ce70f
fabrizio <fcm@unesc.net>
2011-06-10 20:22:34 -0300

    testing ajax changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,12 @@ module ApplicationHelper
   def display_date
     "#{Time.now.strftime("%A %B %d,")} #{Time.now.year}" 
   end
+  
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
 
 end
