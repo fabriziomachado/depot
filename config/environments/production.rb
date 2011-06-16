26aa37dfc89b63170f2c96ef2eb691c5d3b185ab
fabrizio <fcm@unesc.net>
2011-06-16 16:56:59 -0300

    enable sendmail test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Depot::Application.configure do
   
   
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
