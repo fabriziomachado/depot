90cddd94e16ad5dc5fb512e821a9cf478231393f
fabrizio <fcm@unesc.net>
2011-06-16 10:27:38 -0300

    Function Testing Email

--- test/functional/notifier_test.rb
+++ test/functional/notifier_test.rb
@@ -9,16 +9,17 @@ class NotifierTest < ActionMailer::TestCase
     mail = Notifier.order_received(@order)
     assert_equal "Pragmatic Store Order Confirmation", mail.subject
     assert_equal ["dave@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["depot@example.com"], mail.from
     assert_match "Dear Dave Thomas", mail.body.encoded
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "order_shipped" do
     mail = Notifier.order_shipped(@order)
     assert_equal "Pragmatic Store Order Shipped", mail.subject
     assert_equal ["dave@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/, mail.body.encoded
   end
 
 end
