934266d72f55a9ab0cea739c84d01b4d648f64b8
fabrizio <fcm@unesc.net>
2011-06-16 10:41:40 -0300

    big fixes -  email deliver

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Depot::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :test
+  #config.action_mailer.delivery_method = :test
   
 
   # Print deprecation notices to the Rails logger
