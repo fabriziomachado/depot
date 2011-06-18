d8893d7ddcc9dfb12173350434dfeb9d283e78b0
fabrizio <fcm@unesc.net>
2011-06-18 12:48:55 -0300

    Iteration I2: Authenticationg Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,15 @@ class User < ActiveRecord::Base
   attr_accessor :password_confirmation
   
   validate :password_must_be_presente
-  
+
+
+ def User.authenticate(name, password)
+    if user = find_by_name(name)
+      if user.hashed_password == encrypt_password(password, user.salt)
+        user
+      end
+    end
+  end  
     
   def  User.encrypt_password(password, salt)
     Digest::SHA2.hexdigest(password + "wibble" + salt)
