fc6bbdb8114879b512a416203ce790040ca334b6
fabrizio <fcm@unesc.net>
2011-07-02 11:33:22 -0300

    Iteration J3: Translation Checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -59,7 +59,7 @@ class OrdersController < ApplicationController
         session[:cart_id] = nil
         Notifier.order_received(@order).deliver
         
-        format.html { redirect_to(store_url, :notice => 'Tank you for upur order.') }
+        format.html { redirect_to(store_url, :notice => I18n.t('.thanks')) }
         format.xml  { render :xml => @order, :status => :created, :location => @order }
       else
         format.html { render :action => "new" }
