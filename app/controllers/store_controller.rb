274d89d91c5558968bd1a4717ab1cad1699d2e0c
fabrizio <fcm@unesc.net>
2011-07-02 11:56:15 -0300

    Iteration J4: Add a Locale Switcher and migrate to JQuery script

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,9 +3,14 @@ class StoreController < ApplicationController
   skip_before_filter :authorize
   
   def index
-    @count_access_store_index = increment_session_counter
-    @products = Product.all
-    @cart = current_cart
+    
+    if params[:set_locale]
+      redirect_to store_path(:locale => params[:set_locale])
+    else    
+      @count_access_store_index = increment_session_counter
+      @products = Product.all
+      @cart = current_cart
+    end
   end
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+// Append the function to the "document ready" chain
+jQuery(function($) {
+  $('.locale input').each(function(){ $(this).hide()}) 
+})
+
